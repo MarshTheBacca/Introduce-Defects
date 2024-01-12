ee6fb80c5931d0468556c2c9cc0dce8888d11666
MarshTheBacca <marshallhunt@live.com>
2024-01-12 09:12:23 +0000

    renamed to visualise_output.py

--- visualise_output.py
+++ visualise_output.py
@@ -4,11 +4,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 from pathlib import Path
 from utils import NetMCData
-
+import copy
 
 cwd = Path.cwd()
 # netmc_data = NetMCData.from_files(cwd.joinpath("output_files"), "test")
 netmc_data = NetMCData.from_files(cwd.joinpath("output_files"), "test")
-netmc_data.draw_graph()
+netmc_data.draw_graph(True, True, True, True, True, False, False)
+
 plt.gca().set_aspect('equal', adjustable='box')
 plt.show()
+netmc_data.check()
