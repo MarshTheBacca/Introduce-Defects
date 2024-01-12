b88163c66e06612d2f0ad8417238a18310c7de89
MarshTheBacca <marshallhunt@live.com>
2024-01-12 15:38:41 +0000

    adjusted imports temporarliy to import from lammps_data_test.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,5 +1,5 @@
-from .lammps_data import (LAMMPSAngle, LAMMPSAtom, LAMMPSBond, LAMMPSData,
-                          LAMMPSMolecule)
+from .lammps_data_test import (LAMMPSAngle, LAMMPSAtom, LAMMPSBond, LAMMPSData,
+                               LAMMPSMolecule)
 from .netmc_data import (CouldNotBondUndercoordinatedNodesException,
                          InvalidNetworkException,
                          InvalidUndercoordinatedNodesException, NetMCBond,
