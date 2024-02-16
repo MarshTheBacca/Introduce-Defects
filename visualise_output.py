6eeb8c2924993c72a6c850aca30d3df0ef669acd
MarshTheBacca <marshallhunt@live.com>
2024-02-16 16:54:11 +0000

    testing changes

--- visualise_output.py
+++ visualise_output.py
@@ -1,17 +1,51 @@
 # Code to produce coordinates of a lattice of hexagonal nodes and the connections between them
-import networkx as nx
-import numpy as np
 import matplotlib.pyplot as plt
 from pathlib import Path
 from utils import NetMCData
-import copy
-
+import shutil
 cwd = Path(__file__).parent
-netmc_data = NetMCData.from_files(cwd.joinpath("to_lammps"), "gi_test")
-# netmc_data = NetMCData.from_files(cwd.joinpath("Results_12_3194_444_LJ"), "testA")
-# netmc_data = NetMCData.from_scratch(np.array([[0, 0], [10, 10]]), 1)
-netmc_data.draw_graph(True, False, True, False, False, True, False)
+lammps_netmc_path = Path("/home/marshthebacca/LAMMPS-NetMC")
 
+
+# for file_path in cwd.joinpath("test_input").iterdir():
+#     try:
+#         if file_path.is_file() or file_path.is_symlink():
+#             file_path.unlink()
+#         elif file_path.is_dir():
+#             shutil.rmtree(file_path)
+#     except Exception as e:
+#         print(f'Failed to delete {file_path}. Reason: {e}')
+
+# shutil.copy(lammps_netmc_path.joinpath("testing/output_files/test_A_crds.dat"),
+#             cwd.joinpath("test_input/test_A_crds.dat"))
+# shutil.copy(lammps_netmc_path.joinpath("testing/output_files/test_A_aux.dat"),
+#             cwd.joinpath("test_input/test_A_aux.dat"))
+# shutil.copy(lammps_netmc_path.joinpath("testing/output_files/test_A_net.dat"),
+#             cwd.joinpath("test_input/test_A_net.dat"))
+# shutil.copy(lammps_netmc_path.joinpath("testing/output_files/test_A_dual.dat"),
+#             cwd.joinpath("test_input/test_A_dual.dat"))
+# shutil.copy(lammps_netmc_path.joinpath("testing/output_files/test_B_crds.dat"),
+#             cwd.joinpath("test_input/test_B_crds.dat"))
+# shutil.copy(lammps_netmc_path.joinpath("testing/output_files/test_B_aux.dat"),
+#             cwd.joinpath("test_input/test_B_aux.dat"))
+# shutil.copy(lammps_netmc_path.joinpath("testing/output_files/test_B_net.dat"),
+#             cwd.joinpath("test_input/test_B_net.dat"))
+# shutil.copy(lammps_netmc_path.joinpath("testing/output_files/test_B_dual.dat"),
+#             cwd.joinpath("test_input/test_B_dual.dat"))
+
+
+# netmc_data = NetMCData.from_files(cwd.joinpath("test_input"), "test")
+netmc_data = NetMCData.from_files(lammps_netmc_path.joinpath("testing/input_files"), "gi_test")
+# netmc_data = NetMCData.gen_hexagonal(100)
+# netmc_data.scale(2.3)
+print(netmc_data)
+if netmc_data.check():
+    print("Data is consistent")
+else:
+    print("Data is inconsistent")
+    
+print("Average bond length: ", netmc_data.base_network.get_average_bond_length())
+netmc_data.draw_graph(True, True, True, True, True, False, True)
 plt.gca().set_aspect('equal', adjustable='box')
 plt.show()
-netmc_data.check()
+# netmc_data.export(lammps_netmc_path.joinpath("testing/input_files"), "gi_test")
