10992a3a21b29d5661f19b91e95ad3e0c6af03bd
MarshTheBacca <marshallhunt@live.com>
2024-02-27 11:58:12 +0000

    New ways of importing and visualising

--- visualise_output.py
+++ visualise_output.py
@@ -1,60 +1,54 @@
 # Code to produce coordinates of a lattice of hexagonal nodes and the connections between them
 import matplotlib.pyplot as plt
 from pathlib import Path
-from utils import NetMCData
+from utils import NetMCData, LAMMPSData
 import shutil
 
-def visualise_from_lammps():
-    cwd = Path(__file__).parent
-    lammps_netmc_path = cwd.parent.joinpath("LAMMPS-NetMC")
-
-
-    for file_path in cwd.joinpath("test_input").iterdir():
-        try:
-            if file_path.is_file() or file_path.is_symlink():
-                file_path.unlink()
-            elif file_path.is_dir():
-                shutil.rmtree(file_path)
-        except Exception as e:
-            print(f'Failed to delete {file_path}. Reason: {e}')
-
-    shutil.copy(lammps_netmc_path.joinpath("testing/output_files/test_A_crds.dat"),
-                cwd.joinpath("test_input/test_A_crds.dat"))
-    shutil.copy(lammps_netmc_path.joinpath("testing/output_files/test_A_aux.dat"),
-                cwd.joinpath("test_input/test_A_aux.dat"))
-    shutil.copy(lammps_netmc_path.joinpath("testing/output_files/test_A_net.dat"),
-                cwd.joinpath("test_input/test_A_net.dat"))
-    shutil.copy(lammps_netmc_path.joinpath("testing/output_files/test_A_dual.dat"),
-                cwd.joinpath("test_input/test_A_dual.dat"))
-    shutil.copy(lammps_netmc_path.joinpath("testing/output_files/test_B_crds.dat"),
-                cwd.joinpath("test_input/test_B_crds.dat"))
-    shutil.copy(lammps_netmc_path.joinpath("testing/output_files/test_B_aux.dat"),
-                cwd.joinpath("test_input/test_B_aux.dat"))
-    shutil.copy(lammps_netmc_path.joinpath("testing/output_files/test_B_net.dat"),
-                cwd.joinpath("test_input/test_B_net.dat"))
-    shutil.copy(lammps_netmc_path.joinpath("testing/output_files/test_B_dual.dat"),
-                cwd.joinpath("test_input/test_B_dual.dat"))
-
-
-    netmc_data = NetMCData.from_files(cwd.joinpath("test_input"), "test")
-    return netmc_data
-        
-
 cwd = Path(__file__).parent
+lammps_netmc_path = cwd.parent.joinpath("LAMMPS-NetMC")
+images_path = cwd.parent.joinpath("LAMMPS-NetMC/testing/Images")
 
+# NetMC-LAMMPS output
+netmc_data = NetMCData.from_files(lammps_netmc_path.joinpath("testing/output_files"), "test")
 
-netmc_data = visualise_from_lammps()
+# NetMC-LAMMPS input
 # netmc_data = NetMCData.from_files(lammps_netmc_path.joinpath("testing/input_files"), "gi_test")
-# netmc_data = NetMCData.gen_hexagonal(100)
+
+# Introduce defects output
+# netmc_data = NetMCData.from_files(cwd.joinpath("output_files"), "test")
+
+# From scratch
+# netmc_data = NetMCData.gen_hexagonal(256)
 # netmc_data.scale(2.3)
+
 print(netmc_data)
 if netmc_data.check():
     print("Data is consistent")
 else:
     print("Data is inconsistent")
-    
+
+for node in netmc_data.base_network.nodes:
+    if node.num_neighbours != 3:
+        print("Node", node.id, "has", node.num_neighbours, "neighbours")
+        break
 print("Average bond length: ", netmc_data.base_network.get_average_bond_length())
-netmc_data.draw_graph(True, True, True, False, True, False, True)
+
+netmc_data.draw_graph2(True)
+# netmc_data.draw_graph(True, True, True, False, False, True, True)
+
 plt.gca().set_aspect('equal', adjustable='box')
+plt.savefig(images_path.joinpath("network_background.png"), bbox_inches='tight', transparent=False)
 plt.show()
-# netmc_data.export(lammps_netmc_path.joinpath("testing/input_files"), "gi_test")
+# netmc_data.plot_radial_distribution()
+
+#netmc_data.export(cwd.joinpath("test_input"), "test")
+
+
+# output_path = cwd.parent.joinpath("LAMMPS-NetMC/testing/input_files")
+# for file in output_path.iterdir():
+#     if file.is_file() or file.is_symlink():
+#         if file.name != "Si_potential.in" and file.name != "Si.in":
+#             file.unlink()
+# netmc_data.export(output_path, "gi_test")
+# lammps_data = LAMMPSData.from_netmc_network(netmc_data.base_network, "Si", 27.9769265, "molecular")
+# lammps_data.export(output_path.joinpath("Si.data"))
