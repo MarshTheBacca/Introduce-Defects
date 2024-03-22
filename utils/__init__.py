9ad03e87ed5a56075ddb34147277a9b30e74932f
MarshTheBacca <marshallhunt@live.com>
2024-03-22 16:08:30 +0000

    added get_valid_int and get_valid_str

--- utils/__init__.py
+++ utils/__init__.py
@@ -4,3 +4,4 @@ from .netmc_data import (CouldNotBondUndercoordinatedNodesException,
                          InvalidNetworkException,
                          InvalidUndercoordinatedNodesException, NetMCBond,
                          NetMCData, NetMCNetwork, NetMCNode)
+from .validation_utils import get_valid_int, get_valid_str
