bbd35d179bfc60f6f29776cd5713a5f6c2adcb3c
MarshTheBacca <marshallhunt@live.com>
2024-04-15 15:46:26 +0100

    Added UserCancelledError, added get_valid_float by functionalising get_valid_int into get_valid_num. Added clearer doc strings and type hinting

--- utils/__init__.py
+++ utils/__init__.py
@@ -5,4 +5,4 @@ from .bss_data import (BSSBond, BSSData, BSSNetwork, BSSNode,
 from .defect_introducer import DefectIntroducer
 from .lammps_data import (LAMMPSAngle, LAMMPSAtom, LAMMPSBond, LAMMPSData,
                           LAMMPSMolecule)
-from .validation_utils import get_valid_int, get_valid_str
+from .validation_utils import get_valid_int, get_valid_str, get_valid_float, UserCancelledError
--- utils/validation_utils.py
+++ utils/validation_utils.py
@@ -1,10 +1,50 @@
 from typing import Optional
 import sys
 
+from typing import Callable, Optional, TypeVar
+
+NUMBER = TypeVar('NUMBER', int, float)
+
+
+class UserCancelledError(Exception):
+    pass
+
+
+def get_valid_num(prompt: str, convert: Callable[[str], NUMBER], lower: float | int = float("-inf"),
+                  upper: float | int = float("inf"),
+                  exit_string: Optional[str] = None) -> NUMBER:
+    """
+    Obtains a valid number from the user within a given range
+
+    Args:
+        prompt (str): The prompt to display to the user
+        convert (Callable[[str], NUMBER]): The function to convert the input to the desired type
+        lower (float | int): The lower bound of the range
+        upper (float | int): The upper bound of the range
+        exit_string (str): The string to enter to exit the prompt
+    Returns:
+        amswer (NUMBER): The valid input entered by the user
+    Raises:
+        UserCancelledError: If the user cancels the input
+    """
+    while True:
+        answer = input(prompt)
+        if answer == exit_string:
+            raise UserCancelledError("User cancelled input")
+        try:
+            answer = convert(answer)
+        except ValueError:
+            print(f"Answer is not a valid {convert.__name__}")
+            continue
+        if answer < lower or answer > upper:
+            print(f"Answer is out of bounds, must be between {lower} and {upper} inclusive")
+            continue
+        return answer
+
 
 def get_valid_int(prompt: str, lower: float | int = float("-inf"),
                   upper: float | int = float("inf"),
-                  exit_string: Optional[str] = None) -> int | None:
+                  exit_string: Optional[str] = None) -> int:
     """
     Obtains a valid integer from the user within a given range
 
@@ -13,23 +53,31 @@ def get_valid_int(prompt: str, lower: float | int = float("-inf"),
         lower (float | int): The lower bound of the range
         upper (float | int): The upper bound of the range
         exit_string (str): The string to enter to exit the prompt
-
     Returns:
-        int | None: The valid integer entered by the user, or None if the user entered the exit string
+        amswer (int): The valid input entered by the user
+    Raises:
+        UserCancelledError: If the user cancels the input
     """
-    while True:
-        answer = input(prompt)
-        if answer == exit_string:
-            return None
-        try:
-            answer = int(answer)
-        except ValueError:
-            print("Answer is not a valid integer")
-            continue
-        if answer < lower or answer > upper:
-            print(f"Answer is out of bounds, must be between {lower} and {upper} inclusive")
-            continue
-        return answer
+    return get_valid_num(prompt, int, lower, upper, exit_string)
+
+
+def get_valid_float(prompt: str, lower: float | int = float("-inf"),
+                    upper: float | int = float("inf"),
+                    exit_string: Optional[str] = None) -> float:
+    """
+    Obtains a valid float from the user within a given range
+
+    Args:
+        prompt (str): The prompt to display to the user
+        lower (float | int): The lower bound of the range
+        upper (float | int): The upper bound of the range
+        exit_string (str): The string to enter to exit the prompt
+    Returns:
+        amswer (float): The valid input entered by the user
+    Raises:
+        UserCancelledError: If the user cancels the input
+    """
+    return get_valid_num(prompt, float, lower, upper, exit_string)
 
 
 def get_valid_str(prompt: str,
@@ -37,7 +85,7 @@ def get_valid_str(prompt: str,
                   forbidden_chars: Optional[list[str]] = None,
                   lower: int = 0,
                   upper: int = sys.maxsize,
-                  verbose: bool = True) -> str:
+                  exit_string: Optional[str] = None) -> str:
     """
     Gets a valid string from the user
 
@@ -47,23 +95,23 @@ def get_valid_str(prompt: str,
         forbidden_chars (list[str]): A list of forbidden characters
         lower (int): The minimum length of the string
         upper (int): The maximum length of the string
-        verbose (bool): Whether to print error messages
-
+        exit_string (str): The string to enter to exit the prompt
     Returns:
         str: The valid string entered by the user
+    Raises:
+        UserCancelledError: If the user cancels the input
     """
     while True:
         string = input(prompt)
-        if len(string) < lower or len(string) > upper:
-            if verbose:
-                print(f"Input must be between {lower} and {upper} characters long")
+        if string == exit_string:
+            raise UserCancelledError("User cancelled input")
+        elif len(string) < lower or len(string) > upper:
+            print(f"Input must be between {lower} and {upper} characters long")
             continue
-        if allowed_chars and not set(string).issubset(allowed_chars):
-            if verbose:
-                print(f"Input must only contain {allowed_chars}")
+        elif allowed_chars and not set(string).issubset(allowed_chars):
+            print(f"Input must only contain {allowed_chars}")
             continue
-        if forbidden_chars and set(string).intersection(forbidden_chars):
-            if verbose:
-                print(f"Input must not contain {forbidden_chars}")
+        elif forbidden_chars and set(string).intersection(forbidden_chars):
+            print(f"Input must not contain {forbidden_chars}")
             continue
         return string
