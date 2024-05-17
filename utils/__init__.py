cf5b9d7156fff7db3ff22f1a1ef173b0e7128621
MarshTheBacca <marshallhunt@live.com>
2024-05-17 10:46:09 +0100

    added figures version of DefectIntroducer to import list

--- utils/__init__.py
+++ utils/__init__.py
@@ -9,3 +9,5 @@ from .lammps_data import (LAMMPSAngle, LAMMPSAtom, LAMMPSBond, LAMMPSData,
                           LAMMPSMolecule)
 from .validation_utils import (UserCancelledError, confirm, get_valid_float,
                                get_valid_int, get_valid_str)
+
+from .defect_introducer_figures import DefectIntroducerFigures
